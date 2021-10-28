50c7906dda840c3df1f7bae5c08852b1559ba7b1
miotholerus <mio.tholerus@stud.sti.se>
2021-10-28 11:51:11 +0200

    Råkade fippla i main, men funkar nu + styleat knappar

--- ShoppingCartSystem2/src/main/java/com/example/ShoppingCartSystem2/MvcConfig.java
+++ ShoppingCartSystem2/src/main/java/com/example/ShoppingCartSystem2/MvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("store");
+        registry.addViewController("/").setViewName("home");
         //registry.addViewController("/home").setViewName("home");
         registry.addViewController("/store").setViewName("store");
         registry.addViewController("/cart").setViewName("cart");
